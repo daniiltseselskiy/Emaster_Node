ff9ed25a2af306b1dc4b8cf453e8f168c01da570
Houssem Hajlaoui <venatoria@gmail.com>
2018-07-27 07:07:43 +0100

    -

--- deploy.sh
+++ deploy.sh
@@ -22,18 +22,10 @@ EXPOSE 80
 CMD [ "npm start" ]
 EOF
 
-#Cleaning previous runs
-echo "=> Cleaning all containers to save space"
-
-#Stop all containers
-sudo docker stop $(sudo docker ps -a -q)
-
-# Delete all containers
-sudo docker rm $(sudo docker ps -a -q)
-
-# Delete all images
-sudo docker rmi $(sudo docker images -q)
+#Build the Docker container
+docker build -t emasters-connection-api .
+echo "=> Running eMasters Connection API.."
 
 #Run the app within the container
-sudo docker run -d -p 8080:80 --name emasters-connection-api
+sudo docker run -d --name emasters-connection-api
 echo "=> Running eMasters Connection API.."
